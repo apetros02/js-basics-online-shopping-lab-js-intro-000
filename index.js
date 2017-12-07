a21e4a7de0a23c248eda4f648c2d4a610fd84596
alec <alec@petros.audio>
2017-12-07 15:25:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,13 +49,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i=0; i<cart.length; i++) {
+  for (var i = 0; i < cart.length; i++); {
     if (Object.keys(cart[i]) == item) {
       cart = [...cart.slice(0,i), ...cart.slice(i+1)];
       return cart;
     }
   }
-  console.log("That item is not in your cart.");
+  console.log("That item is not in your cart."");
   return cart;
 }
 
