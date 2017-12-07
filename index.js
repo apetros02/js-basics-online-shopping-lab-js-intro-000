e6b96c5bf1189187178a934d973f81716a27d0b4
alec <alec@petros.audio>
2017-12-07 15:06:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function viewCart() {
 function total() {
   var total = 0;
   for (var i = 0; i <= cart.length-1; i++) {
-    total += Object(cart[i]);
+    total += cart[i][Object.keys(cart[i])];
   }
   return console.log(`$` + total);
 }
