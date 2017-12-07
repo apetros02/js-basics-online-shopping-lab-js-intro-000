ae4efcbf458cc4ac4c91b3c492c2ca328e01ba2f
alec <alec@petros.audio>
2017-12-07 14:56:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function total() {
   for (var i = 0; i <= cart.length-1; i++) {
     total += cart[i];
   }
-  return console.log(`$${price}`)
+  return console.log(`$${price}`);
 }
 
 function removeFromCart(item) {
