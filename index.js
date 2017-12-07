b89fab071dfcce26a7dd2adf8d66bb0e49ed72fe
alec <alec@petros.audio>
2017-12-07 14:48:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,20 @@ function viewCart() {
     returnString += `${Object.keys*(cart[1])} at $${cart[0][Object.keys(cart[0])]}.`
   } else if (cart.length === 2) {
     returnString += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`
+  } else {
+    for (var i = 0; i < cart.length; i++) {
+      var prep;
+      if (i === cart.length - 2) {
+        prep = ", and ";
+      } else if (i === cart.length-1) {
+        prep = "";
+      } else {
+        prep = ", ";
+      }
+      returnString += `${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}` + prep + `${i === cart.length - 1 ? '.' : ''}`;
+    }
   }
+  return console.log(returnString)
 }
 
 function total() {
