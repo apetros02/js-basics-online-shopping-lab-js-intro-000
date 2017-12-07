6bcab0681bef5056c848c94c9493f9a7051935cb
alec <alec@petros.audio>
2017-12-07 15:11:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var index = array.indexOf(item);
+  array.splice(index, 1);
 }
 
 function placeOrder(cardNumber) {
