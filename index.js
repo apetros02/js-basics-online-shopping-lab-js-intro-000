0d604979970f509a348b726b19d09aca5316be55
alec <alec@petros.audio>
2017-12-07 15:33:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,5 +63,7 @@ function placeOrder(cardNumber) {
   if (cardNumber == null) {
     return console.log(`Sorry, we don't have a credit card on file for you.`);
   }
-
+  console.log(`Your total cost is $` + total() + `, which will be charged to the card ${cardNumber}.`)
+  cart = [];
+  return
 }
